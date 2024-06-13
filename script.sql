91914b638a21ffc61f048d65f8bd3a828f76cc81
TarlanSB <tarlanworkmail@gmail.com>
2024-06-14 00:43:17 +0300

    Primary keys

--- script.sql
+++ script.sql
@@ -1,12 +1,25 @@
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users
 (
-    username VARCHAR(128) PRIMARY KEY ,
-    firstname VARCHAR(128),
-    lastname VARCHAR(128),
+    id         BIGINT PRIMARY KEY,
+    username   VARCHAR(128) UNIQUE,
+    firstname  VARCHAR(128),
+    lastname   VARCHAR(128),
     birth_date DATE,
-    age INT,
-    role VARCHAR (32),
-    info JSONB
+    role       VARCHAR(32),
+    info       JSONB
 );
+
+CREATE SEQUENCE users_id_seq
+    OWNED BY users.id;
+
+DROP SEQUENCE users_id_seq;
+
+DROP TABLE IF EXISTS users;
+
+--for GenerationType.TABLE  --
+CREATE TABLE all_sequence
+(
+    table_name VARCHAR(32) PRIMARY KEY ,
+    pk_value BIGINT NOT NULL
+);
+
