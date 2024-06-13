29183d40208effd64a6b07b14b4933d019ad55b9
TarlanSB <tarlanworkmail@gmail.com>
2024-06-13 20:56:57 +0300

    Introduction. EntityPersister

--- src/main/java/com/tarlansb/HibernateRunner.java
+++ src/main/java/com/tarlansb/HibernateRunner.java
@@ -5,6 +5,7 @@ import com.tarlansb.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.SQLException;
@@ -12,16 +13,9 @@ import java.sql.SQLException;
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
-//        BlockingDeque<Connection> pool = null;
-//        Connection connection = pool.take();
-//        SessionFactory
-
-//        Connection connection = DriverManager
-//                .getConnection("db.url", "db.username", "db.password");
-//        Session
         Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAnnotatedClass(User.class);
         configuration.addAttributeConverter(new BirthdayConverter());
         configuration.registerTypeOverride(new JsonBinaryType());
         configuration.configure();
@@ -43,18 +37,7 @@ public class HibernateRunner {
 //                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
 //                    .role(Role.ADMIN)
 //                    .build();
-            /**
-            Если отсутствует user, то StateObjectStateException
-             */
-//            session.update(user)
-
-            /**
-             Если отсутствует user, то создать
-             */
-//            session.saveOrUpdate(user)
-
 //            session.delete(user);
-
             User user = session.get(User.class, "ivan@gmail.com");
 
             session.getTransaction().commit();
