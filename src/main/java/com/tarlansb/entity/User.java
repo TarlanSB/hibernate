3b9847be7ca0a11193da353af61ee7c742d2a4ee
TarlanSB <tarlanworkmail@gmail.com>
2024-06-15 23:29:46 +0300

    Fetch types

--- src/main/java/com/tarlansb/entity/User.java
+++ src/main/java/com/tarlansb/entity/User.java
@@ -1,10 +1,7 @@
 package com.tarlansb.entity;
 
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import javax.persistence.GeneratedValue;
@@ -17,6 +14,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "company")
 @Builder
 @Entity
 @Table(name = "users", schema = "public")
@@ -39,7 +37,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @ManyToOne
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id") // company_id
     private Company company;
 }
