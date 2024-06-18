525e73aac98068af75c70b3242777629a46179b9
TarlanSB <tarlanworkmail@gmail.com>
2024-06-18 23:18:08 +0300

    ManyToMany

--- script.sql
+++ script.sql
@@ -4,6 +4,27 @@ DROP TABLE IF EXISTS company;
 
 DROP TABLE IF EXISTS profile;
 
+CREATE TABLE chat
+(
+    id BIGSERIAL PRIMARY KEY ,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+
+CREATE TABLE users_chat
+(
+    user_id BIGINT REFERENCES users (id),
+    chat_id BIGINT REFERENCES chat (id),
+    PRIMARY KEY (user_id, chat_id)
+);
+
+CREATE TABLE profile
+(
+    id BIGSERIAL PRIMARY KEY ,
+    user_id BIGINT NOT NULL UNIQUE REFERENCES users (id),
+    street VARCHAR(128),
+    language CHAR(2)
+);
+
 CREATE TABLE profile
 (
     id BIGSERIAL PRIMARY KEY ,
---
+++ src/main/java/com/tarlansb/entity/Chat.java
@@ -0,0 +1,28 @@
+package com.tarlansb.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "name")
+@ToString(exclude = "users")
+@Builder
+@Entity
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "chats")
+    private Set<User> users = new HashSet<>();
+}
